b364e1c86b62d7e881cee4d2eb7184da21f42784
mohamed ahmed samir <105310173+Mohamed-samir-504@users.noreply.github.com>
2023-09-14 19:43:13 +0300

    ..

---
+++ Unit 8 MCU interfacing/lesson 5 section/ATMEGA32_SPI_Driver/ATMEGA32_Drivers/Debug/MCAL/SPI_Driver.d
@@ -0,0 +1,21 @@
+MCAL/SPI_Driver.d MCAL/SPI_Driver.o: ../MCAL/SPI_Driver.c \
+ ../MCAL/inc/SPI_Driver.h ../MCAL/inc/ATMEGA32.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ ../MCAL/inc/GPIO_Driver.h
+
+../MCAL/inc/SPI_Driver.h:
+
+../MCAL/inc/ATMEGA32.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+../MCAL/inc/GPIO_Driver.h:
---
+++ Unit 8 MCU interfacing/lesson 5 section/ATMEGA32_SPI_Driver/ATMEGA32_Drivers/Debug/MCAL/USART_Driver.d
@@ -0,0 +1,21 @@
+MCAL/USART_Driver.d MCAL/USART_Driver.o: ../MCAL/USART_Driver.c \
+ ../MCAL/inc/USART_Driver.h ../MCAL/inc/ATMEGA32.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ ../MCAL/inc/GPIO_Driver.h
+
+../MCAL/inc/USART_Driver.h:
+
+../MCAL/inc/ATMEGA32.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+../MCAL/inc/GPIO_Driver.h:
